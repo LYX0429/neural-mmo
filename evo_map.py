437be41cada710737fb644897c1207c5204fdc0f
Sam Earle <smearle93@gmail.com>
2020-11-24 03:09:56 -0500

    Reorganize evolution code.

--- evo_map.py
+++ evo_map.py
@@ -10,6 +10,7 @@ import time
 import json
 from multiprocessing import Pipe, Process
 from shutil import copyfile
+from evolution.lambda_mu import LambdaMuEvolver
 
 import numpy as np
 
@@ -36,7 +37,6 @@ np.set_printoptions(threshold=sys.maxsize,
                     )
 import torch
 
-MULTIPROCESSING = False
 
 
 
@@ -122,313 +122,6 @@ def createPolicies(config):
     return policies
 
 
-class LambdaMuEvolver():
-    def __init__(
-            self,
-            save_path,
-           #make_env,  # a function that creates the environment
-            n_pop=3,
-            lam=1 / 3,
-            mu=1 / 3,
-            n_proc=12,
-            n_epochs= 10000
-    ):
-        self.genes = {}
-        self.save_path = save_path
-        self.evolver_path = os.path.join(self.save_path, 'evolver')
-        self.log_path = os.path.join(self.save_path, 'log.csv')
-        self.n_proc = n_proc
-       #self.make_env = make_env
-        self.lam = lam
-        self.mu = mu
-        self.n_pop = n_pop
-        self.n_epochs = n_epochs
-        self.n_sim_ticks = 100
-        self.population = {}  # hash: (game, score, age)
-        self.max_skills = {}
-        self.n_tick = 0
-        self.n_gen = 0
-        self.score_hists = {}
-        self.n_epoch = 0
-        self.n_pop = n_pop
-        self.g_hashes = list(range(self.n_pop))
-        self.n_init_builds = 30
-        self.n_mutate_actions = self.n_init_builds
-        self.mature_age = 1
-
-    def join_procs(self, processes):
-
-        for g_hash, (game, score_t, age) in self.population.items():
-            # get score from latest simulation
-            # cull score history
-
-            if len(self.score_hists[g_hash]) >= 100:
-                while len(self.score_hists[g_hash]) >= 100:
-                    self.score_hists[g_hash].pop(0)
-            # hack
-            if score_t is None:
-                score_t = 0
-            else:
-                self.score_hists[g_hash].append(score_t)
-                score = np.mean(self.score_hists[g_hash])
-                game, _, age = self.population[g_hash]
-                self.population[g_hash] = (game, score, age + 1)
-
-    def evolve_generation(self):
-        print('epoch {}'.format(self.n_epoch))
-        population = self.population
-        processes = {}
-        n_proc = 0
-
-        for g_hash, (game, score, age) in population.items():
-            map_arr = self.genes[g_hash]
-            if MULTIPROCESSING:
-                parent_conn, child_conn = Pipe()
-                p = Process(target=self.simulate_game,
-                        args=(
-                            game,
-                            map_arr,
-                            self.n_sim_ticks,
-                            child_conn,
-                            ))
-                p.start()
-                processes[g_hash] = p, parent_conn, child_conn
-            else:
-                # NB: specific to NMMO!!
-                # we simulate a bunch of envs simultaneously through the rllib trainer
-                self.simulate_game(game, map_arr, self.n_sim_ticks, g_hash=g_hash)
-                parent_conn, child_conn = None, None
-                processes[g_hash] = score, parent_conn, child_conn
-                for g_hash, (game, score, age) in population.items():
-                    try:
-                        with open(os.path.join('./evo_experiment', '{}'.format(self.config['config'].EVO_DIR), 'env_{}_skills.json'.format(g_hash))) as f:
-                            agent_skills = json.load(f)
-                            score = self.update_entropy_skills(agent_skills)
-                    except FileNotFoundError:
-                        raise Exception
-                        # hack
-                        score = None
-                        processes[g_hash] = score, parent_conn, child_conn
-                    self.population[g_hash] = (game, score, age)
-
-                n_proc += 1
-                break
-
-        self.join_procs(processes)
-        if n_proc % self.n_proc == 0:
-            self.join_procs(processes)
-
-        if len(processes) > 0:
-            self.join_procs(processes)
-
-        return self.mutate_gen()
-
-    def mutate_gen(self):
-        population = self.population
-        n_cull = int(self.n_pop * self.mu)
-        n_parents = int(self.n_pop * self.lam)
-        dead_hashes = []
-        pop_list = [(g_hash, game, score, age) for g_hash, (game, score, age) in self.population.items() if score is not None] 
-        ranked_pop = sorted(pop_list, key=lambda tpl: tpl[2])
-        print('Ranked population: (id, running_mean_score, last_score, age)')
-
-        with open(self.log_path, mode='a') as log_file:
-            log_writer = csv.writer(
-                    log_file, delimiter=',',
-                    quotechar='"',
-                    quoting=csv.QUOTE_MINIMAL)
-            log_writer.writerow(['epoch {}'.format(self.n_epoch)])
-            log_writer.writerow(
-                    ['id', 'running_score', 'last_score', 'age'])
-
-            for g_hash, game, score, age in ranked_pop:
-
-                if g_hash in self.score_hists:
-                    score_hist = self.score_hists[g_hash]
-                    if len(score_hist) > 0:
-                        last_score = self.score_hists[g_hash][-1]
-                    else:
-                        last_score = 0
-                else:
-                    last_score = -1
-                print('{}, {:2f}, {:2f}, {}'.format(
-                    g_hash, score, last_score, age))
-                log_writer.writerow([g_hash, score, last_score, age])
-
-        for j in range(n_cull):
-            dead_hash = ranked_pop[j][0]
-
-            if self.population[dead_hash][2] > self.mature_age:
-                dead_hashes.append(dead_hash)
-
-        par_hashes = []
-
-        for i in range(n_parents):
-            par_hash, _, _, age = ranked_pop[-(i + 1)]
-
-            if age > self.mature_age:
-                par_hashes.append(par_hash)
-
-        # for all we have culled, add new mutated individual
-        j = 0
-
-        mutated = []
-        if par_hashes:
-            while dead_hashes:
-                n_parent = j % len(par_hashes)
-                par_hash = par_hashes[n_parent]
-                # parent = population[par_hash]
-                par_map = self.genes[par_hash]
-                # par_game = parent[0]  # get game from (game, score, age) tuple
-                g_hash = dead_hashes.pop()
-                mutated.append(g_hash)
-                population.pop(g_hash)
-               #self.score_var.pop(g_hash)
-                child_map = self.mutate(par_map)
-               #child_game = self.make_game(child_map)
-                child_game = None
-                population[g_hash] = (child_game, None, 0)
-                self.genes[g_hash] = child_map
-                self.score_hists[g_hash] = []
-                j += 1
-
-        self.saveMaps(self.genes, mutated)
-
-        if self.n_epoch % self.mature_age == 0 or self.n_epoch == 2:
-            self.save()
-        self.n_epoch += 1
-
-
-    def save(self):
-        save_file = open(self.evolver_path, 'wb')
-        copyfile(self.evolver_path, self.evolver_path + '.bkp')
-        pickle.dump(self, save_file)
-
-
-
-    def init_pop(self):
-
-        for i in range(self.n_pop):
-            rank= self.g_hashes.pop()
-
-           #if rank in self.genes:
-           #    map_arr = self.genes[rank]
-           #else:
-            map_arr = self.genRandMap()
-            game = None
-            self.population[rank]= (game, None, 0)
-            self.genes[rank]= map_arr
-            self.score_hists[rank] = []
-
-        try:
-            os.mkdir(os.path.join(self.save_path, 'maps'))
-        except FileExistsError:
-            print('Overwriting evolved maps at {}.'.format(self.save_path))
-        self.saveMaps(self.genes, list(self.genes.keys()))
-        print('restoring')
-        self.restore()
-        print('restoring')
-
-    def evolve(self, n_epochs=None):
-    #   self.save()
-    #   raise Exception
-        if n_epochs:
-            self.n_epochs = n_epochs
-        if self.n_epoch == 0:
-            self.init_pop()
-
-
-        while self.n_epoch < self.n_epochs:
-            start_time= time.time()
-            self.evolve_generation()
-            time_elapsed= time.time() - start_time
-            hours, rem= divmod(time_elapsed, 3600)
-            minutes, seconds= divmod(rem, 60)
-            print("time elapsed: {:0>2}:{:0>2}:{:05.2f}".format(
-                int(hours), int(minutes), seconds))
-
-    def genRandMap(self):
-        raise NotImplementedError()
-
-    def mutate(self, map_arr):
-        raise NotImplementedError()
-
-    def restore():
-        ''' Use saved maps to instantiate games. '''
-        raise NotImplementedError()
-
-    def infer(self):
-        raise NotImplementedError()
-
-
-class SkillEvolver(LambdaMuEvolver):
-    def __init__(self, *args, **kwargs):
-        alpha = kwargs.pop('alpha')
-        self.alpha = alpha
-        super().__init__(*args, **kwargs)
-
-    def infer(self):
-        for g_hash, (_, score, age) in self.population.items():
-            agent_skills = self.genes[g_hash]
-            print(np.array(agent_skills))
-            print('score: {}, age: {}'.format(score, age))
-
-    def genRandMap(self):
-      # agent_skills = [
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-      #     [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.], ]
-        agent_skills = [[0 for i in range(3)] for j in range(8)]
-
-        return agent_skills
-
-    def simulate_game(self,
-                        game,
-                        agent_skills,
-                        n_sim_ticks,
-                        child_conn,):
-        score = calc_diversity_l2(agent_skills, self.alpha)
-
-        if child_conn:
-            child_conn.send(score)
-
-        return score
-
-    def mutate(self, gene):
-        agent_skills = copy.deepcopy(gene)
-        n_agents = len(agent_skills)
-        for i in range(random.randint(1, 5)):
-            for j in range(i):
-                a_i = random.randint(0, n_agents - 1)
-                s_i = random.randint(0, len(agent_skills[0]) - 1)
-               #if s_i in [0, 5, 6]:
-               #    min_xp = 1000
-               #else:
-                min_xp = 0
-                agent_skills[a_i][s_i] = \
-                        min(max(min_xp, agent_skills[a_i][s_i] + random.randint(-100, 100)), 20000)
-
-        if n_agents > 1 and random.random() < 0.05:
-            # remove agent
-            agent_skills.pop(random.randint(0, n_agents - 1))
-            n_agents -= 1
-        if 8 > n_agents > 0 and random.random() < 0.05:
-            # add agent
-            agent_skills.append(copy.deepcopy(agent_skills[random.randint(0, n_agents - 1)]))
-
-
-        return agent_skills
-
-    def make_game(self, agent_skills):
-        return None
-
-    def restore(self, **kwargs):
-        pass
 
 
 
@@ -700,6 +393,8 @@ class EvolverNMMO(LambdaMuEvolver):
         if not g_hash:
             raise Exception('No population found for inference.')
         game = self.population[g_hash][0]
+        if game is None:
+            game = self.make_env(self.config)
         map_arr = self.genes[g_hash]
        #map_arr = self.genRandMap()
         self.run(game, map_arr)
@@ -882,6 +577,7 @@ class EvolverNMMO(LambdaMuEvolver):
                             agent_skills.append(row)
                         i += 1
                     score = self.update_entropy_skills(agent_skills)
+                assert csv_skills.closed
                 self.population[g_hash] = (game, score, age)
             except FileNotFoundError:
                 T()
@@ -910,7 +606,7 @@ class EvolverNMMO(LambdaMuEvolver):
 
     def tick(self, game=None, g_hash=None):
         # check if we are doing inference
-        FROZEN = False
+        FROZEN = True
         if FROZEN:
             if game is None:
                 game = self.game
@@ -1047,148 +743,4 @@ def calc_diversity(agent_skills, alpha, skill_idxs=None):
 
     return score
 
-def some_examples():
-   #print('bad situation')
-    agent_skills = [
-            [0, 0, 0],
-            ]
-    ent = calc_diversity(agent_skills, 0.5)
 
-   #print('less bad situation')
-    agent_skills = [
-            [0, 0, 10],
-            ]
-    ent = calc_diversity(agent_skills, 0.5)
-
-    agent_skills = [
-            [0, 0, 0],
-            [0, 0, 10],
-            ]
-    ent = calc_diversity(agent_skills, 0.5)
-
-    agent_skills = [
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 10],
-            ]
-    ent = calc_diversity(agent_skills, 0.5)
-
-
-    print('NMMO: pacifism')
-    agent_skills = [
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200.,   0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],]
-    ent = calc_diversity(agent_skills)
-
-    print('NMMO: a skirmish and one murder')
-    agent_skills = [
-    [1200.,  0.,  0. ,  0. ,  0. , 1500.,1500. ,  0. ,  0.],
-    [1200. ,  0.,1080.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200. ,  0., 240.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200. ,  0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200. ,  0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200. ,  0.,   0.,   0., 240.,1500.,1500.,   0.,   0.],
-    [1200. ,  0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-    [1200. ,  0.,   0.,   0.,   0.,1500.,1500.,   0.,   0.],
-   ]
-    ent = calc_diversity(agent_skills)
-
-    print('NMMO: a skirmish')
-    agent_skills = [
-    [1200.,   0., 120. ,  0.,  0., 1500.,1500. ,  0.,   0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,120.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],]
-    ent = calc_diversity(agent_skills)
-
-    print('NMMO: a skirmish, smaller population')
-    agent_skills = [
-    [1200.,   0., 1200. ,  0.,  0.,1500.,1500. ,  0.,   0.],
-    [1200. ,  0. ,  0.  , 240., 0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,120.,1500.,1500.  , 0. ,  0.],
-    [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],
-#   [1200. ,  0. ,  0.  , 0. ,  0.,1500.,1500.  , 0. ,  0.],]
-]
-    ent = calc_diversity(agent_skills)
-
-if __name__ == '__main__':
-#   some_examples()
-    parser= argparse.ArgumentParser()
-    parser.add_argument('--experiment-name',
-                        default='scratch',
-                        help='name of the experiment')
-    parser.add_argument('--load',
-                        default=False,
-                        action='store_true',
-                        help='whether or not to load a previous experiment')
-    parser.add_argument('--n-pop',
-                        type=int,
-                        default=12,
-                        help='population size')
-    parser.add_argument('--lam',
-                        type=float,
-                        default=1 / 3,
-                        help='number of reproducers each epoch')
-    parser.add_argument(
-                        '--mu',
-                        type=float,
-                        default=1 / 3,
-                        help='number of individuals to cull and replace each epoch')
-    parser.add_argument('--inference',
-                        default=False,
-                        action='store_true',
-                        help='watch simulations on evolved maps')
-    parser.add_argument('--n-epochs',
-                        default=10000,
-                        type=int,
-                        help='how many generations to evolve')
-    parser.add_argument('--alpha',
-                        default=0.66,
-                        type=float,
-                        help='balance between skill and agent entropies')
-    args = parser.parse_args()
-    n_epochs = args.n_epochs
-    experiment_name = args.experiment_name
-    load = args.load
-
-
-    save_path = 'evo_experiment/{}'.format(args.experiment_name)
-
-    if not os.path.isdir(save_path):
-        os.mkdir(save_path)
-    try:
-        evolver_path = os.path.join(save_path, 'evolver')
-        with open(evolver_path, 'rb') as save_file:
-            evolver = pickle.load(save_file)
-            print('loading evolver from save file')
-        evolver.restore(trash_data=True)
-    except FileNotFoundError as e:
-        print(e)
-        print('no save file to load')
-
-        evolver = SkillEvolver(save_path,
-            n_pop=12,
-            lam=1 / 3,
-            mu=1 / 3,
-            n_proc=12,
-            n_epochs=n_epochs,
-            alpha=args.alpha,
-        )
-
-    if args.inference:
-        evolver.infer()
-    else:
-        evolver.evolve(n_epochs=n_epochs)
