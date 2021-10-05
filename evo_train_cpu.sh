449b1245218c979a36757c65ee461d4bed523f87
Sam Earle <smearle93@gmail.com>
2021-10-05 13:14:38 -0400

    change

--- evo_train_cpu.sh
+++ evo_train_cpu.sh
@@ -3,18 +3,18 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 ##SBATCH --gres=gpu:1
-#SBATCH --time=12:00:00
+#SBATCH --time=3:00:00
 #SBATCH --mem=50GB
-#SBATCH --job-name=nmmo_0
+#SBATCH --job-name=nmmo0_0
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --mail-user=sam.earle@nyu.edu
-#SBATCH --output=nmmo_0_%j.out
+#SBATCH --mail-user=yl8616@nyu.edu
+#SBATCH --output=nmmo0_0_%j.out
 
-cd /scratch/se2161/neural-mmo || exit
+# cd /scratch/se2161/neural-mmo || exit
 
 #conda init bash
 source activate
-conda activate nmmo
+conda activate nmmo0
 
 export TUNE_RESULT_DIR='./evo_experiment/'
 python ForgeEvo.py --load_arguments 0
